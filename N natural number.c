8acaba00d6712d2c7067b84016596d1e333973e9
thaya47 <48437334+thaya47@users.noreply.github.com>
2019-08-11 08:27:40 +0530

    N natural number.c

---
+++ N natural number.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+
+int main()
+{
+    int N,a;
+    printf("enter the value of N");
+    scanf("%d",&N);
+    for(int i=0;i<=N;i++)
+    {
+        a=i+a;
+       
+    }
+     printf("%d",a);
+    getch();
+    }

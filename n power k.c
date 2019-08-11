49cfb24458134b679bdcd79c0ff20c93182ec070
thaya47 <48437334+thaya47@users.noreply.github.com>
2019-08-11 12:11:35 +0530

    n power k.c

---
+++ n power k.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+int main()
+{
+    int N,K,a;
+    printf("enter the value of N");
+    scanf("%d",&N);
+    printf("enter the value of K");
+    scanf("%d",&K);
+    a=pow(N,K);
+    printf("%d",a);
+    getch();
+}
+    
